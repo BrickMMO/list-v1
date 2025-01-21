5dc412f8f4dcdc360afc9481e6ea1a56093dabb3
Adam <thomasadam83@hotmail.com>
2025-01-21 10:46:02 -0500

    Added email validation to signup form

--- signup.php
+++ signup.php
@@ -18,13 +18,34 @@ include('includes/connect.php');
 
         Email:
         <br>
-        <input type="email" name="email">
+        <input type="email" name="email" id="email">
+        <div id="email-error"></div>
 
         <br>
 
-        <input type="submit" value="Sign Up">
+        <input type="submit" value="Sign Up" onclick="return validateForm()">
 
     </form>
+
+    <script>
+    function validateForm() {
+        let errors = 0;
+
+        let email_pattern = "[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$";
+        let email = document.getElementById("email");
+        let email_error = document.getElementById("email-error");
+        email_error.innerHTML = "";
+        if (email.value == "") {
+            email_error.innerHTML = "(email is required)";
+            errors++;
+        } else if (!email.value.match(email_pattern)) {
+            email_error.innerHTML = "(email is invalid)";
+            errors++;
+        }
+
+        if (errors) return false;
+    }
+</script>
 
 </body>
 </html>
