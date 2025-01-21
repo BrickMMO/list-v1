9593fd3c848d67d21fcf95660ed5d1b28bf55020
Adam <thomasadam83@hotmail.com>
2025-01-21 10:40:13 -0500

    Added htaccess

--- signup.php
+++ signup.php
@@ -1,13 +1,30 @@
+<?php
+
+include('includes/connect.php');
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Sign Up</title>
 </head>
 <body>
     
     <h1>Sign Up</h1>
+
+    <form method="post">
+
+        Email:
+        <br>
+        <input type="email" name="email">
+
+        <br>
+
+        <input type="submit" value="Sign Up">
+
+    </form>
 
 </body>
 </html>
